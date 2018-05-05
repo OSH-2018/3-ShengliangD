72bead211c4541c257d390edafad97c9a60eed1c
shengliangd <shengliangd@outlook.com>
2018-05-05 23:46:42 +0800

    init

--- params.h
+++ params.h
@@ -2,9 +2,14 @@
 #define PARAMS_H
 
 #define FS_SIZE (4 * 1024 * 1024 * 1024ULL)
+
 #define BLOCK_SIZE (4 * 1024)
 #define BLOCK_NUM (FS_SIZE / BLOCK_SIZE)
+
+#define BMBLOCK_BEGIN 1
 #define BMBLOCK_NUM (BLOCK_NUM / 8 / BLOCK_SIZE)
+
+#define IBLOCK_BEGIN (1 + BM_BLOCK_NUM)
 #define IBLOCK_NUM 2
 
 #endif
--- sffs.c
+++ sffs.c
@@ -3,17 +3,43 @@
 #include <errno.h>
 #include <fuse.h>
 #include <sys/mman.h>
+#include <assert.h>
 
 #include "params.h"
 
-void * media[BLOCK_NUM];
+typedef unsigned long long ull;
+
+void * blocks[BLOCK_NUM];
+
+void * new_block() {
+    void * ret;
+    assert ( (ret = mmap(NULL, BLOCK_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0)) != MAP_FAILED );
+    memset(ret, 0, BLOCK_SIZE);
+    return ret;
+}
+
+void delete_block(void * ptr) {
+    assert ( munmap(ptr, BLOCK_SIZE) != -1 );
+}
 
 static void *sffs_init(struct fuse_conn_info *conn) {
     // Fill block 0, ie. the summary of this file system
-    //
+    blocks[0] = new_block();
+    ((ull*)blocks[0])[0] = BLOCK_SIZE;
+    ((ull*)blocks[0])[1] = BLOCK_NUM;
+    ((ull*)blocks[0])[2] = 1 + BMBLOCK_NUM + IBLOCK_NUM;
+
     // Fill bitmap blocks
-    //
+    for (uul i = 0; i < BMBLOCK_NUM; ++i) {
+        blocks[BMBLOCK_BEGIN + i] = new_block();
+    }
+
     // Fill index blocks
+    for (uul i = 0; i < IBLOCK_NUM; ++i) {
+        blocks[IBLOCK_BEGIN + i] = new_block();
+    }
+
+    return NULL;
 }
 
 static const struct fuse_operations op = {
